c26575b53411160b2eae8518c27f34022c983a69
Arteww <arrmf1@mail.ru>
2022-06-05 17:23:18 +0700

    1st lesson

--- json_data/open_json.py
+++ json_data/open_json.py
@@ -1,7 +1,7 @@
 import os
 from json import load
 
-path_to_pictures_json = f'{os.path.dirname(os.path.abspath(__file__))}\cards.json'
+path_to_pictures_json = f'{os.path.dirname(os.path.abspath(__file__))}/cards.json'
 
 with open(path_to_pictures_json, 'r', encoding="utf-8") as file:
     cards = load(file)
