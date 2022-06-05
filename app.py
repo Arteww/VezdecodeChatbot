cdfe6696c847a55e3b6b9755e888e64b6173f838
Arteww <arrmf1@mail.ru>
2022-06-05 17:22:11 +0700

    1st lesson

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from vkbottle import Bot
 
 from blueprints import bps
 
-# from middlewares import EventSpamMiddleware, MessageSpamMiddleware
-# from states import state_dispenser
 from config import api, db
 
 
@@ -13,22 +11,17 @@ def setup_blueprints(bot_: Bot):
 
 
 def setup_middlewares(bot_: Bot):
-    # bot_.labeler.raw_event_view.register_middleware(EventSpamMiddleware())
-    # bot_.labeler.message_view.register_middleware(MessageSpamMiddleware())
     pass
 
 
 def init_bot():
     bot_ = Bot(api=api)
-    # bot_.state_dispenser = state_dispenser
     setup_middlewares(bot_)
     setup_blueprints(bot_)
     return bot_
 
 
 async def init_db():
-    # await db.create()
-    # await db.create_table_users()
     pass
 
 
